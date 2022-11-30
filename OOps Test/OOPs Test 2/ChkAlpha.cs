1fc3e58866468c6d46d21f6f045d0b725ea3cf33
Abhijeet Birajdar <98395644+Abhijeet44@users.noreply.github.com>
2022-11-30 11:20:17 +0530

    Add files via upload

---
+++ OOps Test/OOPs Test 2/ChkAlpha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myfirstproject.OOPs_Test_2
+{
+    class ChkAlpha
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Alphabet : ");
+            char c = Convert.ToChar(Console.ReadLine());
+
+            if(c >= 'a' && c <= 'z')
+            {
+                Console.WriteLine("Lower case");
+            }
+            else if(c >= 'A' && c <= 'Z')
+            {
+                Console.WriteLine("Upper case");
+            }
+            else
+            {
+                Console.WriteLine("NOT an Alphabet");
+            }
+        }
+    }
+}
---
+++ OOps Test/OOPs Test 2/TrimorphicNum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myfirstproject.OOPs_Test_2
+{
+    class TrimorphicNum
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Numbber To Check : ");
+            int num = int.Parse(Console.ReadLine());
+
+            int cube = num * num * num;
+
+            if (cube % 10 == num % 10) 
+                Console.WriteLine("TRIMORPHIC");
+            else
+                Console.WriteLine("NOT TRIMORTHIC");
+        }
+    }
+}
